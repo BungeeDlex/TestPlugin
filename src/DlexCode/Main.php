fa3bf427f4e6beb4557d2cd004652579883afb25
BungeeDlex <46621982+BungeeDlex@users.noreply.github.com>
2019-01-24 15:51:32 +0100

    Main create!

---
+++ src/DlexCode/Main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DlexCode;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PLuginBase {
+    public function onEnable() {
+        $this->getLogger()-info("Test PLugin wurde geladen!");
+        }
+        
+}     
